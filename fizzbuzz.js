0292240ef2eaa06cf5cad6f77f3e25ab3c086d95
freyalindley <57677490+freyalindley@users.noreply.github.com>
2019-11-19 20:16:40 +0000

    add bracket

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -5,15 +5,16 @@ for ( var i = 1; i <= 100; i++ ){
     
     if ( i % 3 == 0){
 
-output ("Fizz");
+    output ("Fizz");
+
+    }
+
+    else if ( i % 5 == 0 ) {
+        output("Buzz");
+    }
+
+    else {
+        output ( i );
+    }
 
 }
-
-else if ( i % 5 == 0 ) {
-    output("Buzz");
-}
-
-else {
-    output ( i );
-}
-
