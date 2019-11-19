a79304a49fdbf01d0e28743084014a6ca621cddf
freyalindley <57677490+freyalindley@users.noreply.github.com>
2019-11-19 20:27:45 +0000

    updated buzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -3,11 +3,16 @@
 
 for ( var i = 1; i <= 100; i++ ){
     
-    if ( i % 3 == 0){
+    if ( i % 3 == 0 && i % 5 == 0 ){
 
-    output ("Fizz");
+    output ("FizzBuzz");
 
     }
+
+    else if ( i % 3 == 0 ) {
+        
+    }
+
 
     else if ( i % 5 == 0 ) {
         output("Buzz");
